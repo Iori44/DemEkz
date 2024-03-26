7ea4c6d55dd37dd6963fd613aa62ec7b8c138e44
Iori44 <133634347+Iori44@users.noreply.github.com>
2024-03-26 10:45:36 +0500

    Add files via upload

---
+++ Useers/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Useers
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var query = (from Users in zxcEntities.GetContext().Users where Users.Login == tbLogin.Text && Users.Passwowrd == tbPassword.Password select Users).First();
+
+                
+                    if (query.Login == "admin1") 
+                    
+                    {
+                        Admin adminWindow = new Admin();
+                        adminWindow.Show();
+                    }
+                    else
+                    {
+                        // Show User Window
+                        User userWindow = new User();
+                        userWindow.Show();
+                    }
+                    this.Close();
+                
+                
+            }
+            catch 
+            {
+                MessageBox.Show("An error occurred:");
+            }
+        }
+
+        private void LoginWindow_Closed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
